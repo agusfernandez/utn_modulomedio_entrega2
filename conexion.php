d3b50036a8ba2425969f74ac237564a0553c0103
agusfernandez7 <agustina.fernandez@improntus.com>
2023-06-22 00:26:34 -0300

    cambio en conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conexion_db = mysqli_connect('localhost' , 'root' , '123456', 'entrega3')  or exit ('No se pudo hacer la conexion de forma correcta');
+$conexion_db = mysqli_connect('localhost' , 'root' , '123456', 'phpintermedio')  or exit ('No se pudo hacer la conexion de forma correcta');
