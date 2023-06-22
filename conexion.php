dc9bd0f8576aa16cca8b5a83bc16355444d48ec6
agusfernandez7 <agustina.fernandez@improntus.com>
2023-06-22 17:26:55 -0300

    cambio de data de conexion

--- conexion.php
+++ conexion.php
@@ -1,3 +1,5 @@
 <?php 
 
-$conexion_db = mysqli_connect('localhost' , 'root' , '123456', 'phpintermedio')  or exit ('No se pudo hacer la conexion de forma correcta');
+$conexion_db = mysqli_connect('localhost' , 'id20486541_entregacuatro' , 'k(~c3E{C{YUZ-9hW', 'id20486541_phpintermedio')  or exit ('No se pudo hacer la conexion de forma correcta');
+
+//$conexion_db = mysqli_connect('localhost' , 'root' , '123456', 'phpintermedio')  or exit ('No se pudo hacer la conexion de forma correcta');
